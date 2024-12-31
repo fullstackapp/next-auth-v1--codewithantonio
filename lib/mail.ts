71d4ab26726d42c55a7f9d8924837d1c8c0620a9
Full Stack App <forfullstackdev@gmail.com>
2024-12-31 10:53:48 +0300

    Change mail

--- lib/mail.ts
+++ lib/mail.ts
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmationLink = `${process.env.NEXTAUTH_URL}/auth/new-verification?token=${token}`;
   await resend.emails.send({
-    from: 'onboarding@playscript.app',
+    from: 'onboarding@resend.dev',
     to: email,
     subject: 'Confirm your email',
     html: `<p>Please click <a href="${confirmationLink}">here</a> to confirm your email address.</p>`,
@@ -15,7 +15,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
 export const sendPasswordResetEmail = async (email: string, token: string) => {
   const passwordResetLink = `${process.env.NEXTAUTH_URL}/auth/new-password?token=${token}`;
   await resend.emails.send({
-    from: 'onboarding@playscript.app',
+    from: 'onboarding@resend.dev',
     to: email,
     subject: 'New password',
     html: `<p>Please click <a href="${passwordResetLink}">here</a> to reset your password.</p>`,
@@ -24,7 +24,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
 
 export const sendTwoFactorEmail = async (email: string, token: string) => {
   await resend.emails.send({
-    from: 'onboarding@playscript.app',
+    from: 'onboarding@resend.dev',
     to: email,
     subject: '2FA Code',
     html: `<p>Your 2FA code: ${token}</p>`,
